2c16ed1bd511d4b5bddf8d9a281098c2f017ab45
mathias <mathias@weidner.in-bad-schmiedeberg.de>
2014-03-06 07:50:18 +0000

    Zeilenzahl angepasst, dass es auf eine Seite passt

--- code/fnotify.c
+++ code/fnotify.c
@@ -1,24 +1,16 @@
-/*
- * file_access_notify.c - notify about file access
- */
-
+/* fnotify.c - notify about file access */
 #include <linux/fcntl.h>
 #include <stdio.h>
 #include <sys/fanotify.h>
 
 int main(int arg, char **argv) {
-
     struct fanotify_event_metadata ev;
-
     int fa = fanotify_init(FAN_CLASS_NOTIF,
 	O_RDONLY | O_LARGEFILE);
-
     if (fa >= 0) {
-	int m = fanotify_mark(fa,
-	    FAN_MARK_ADD,
+	int m = fanotify_mark(fa, FAN_MARK_ADD,
 	    FAN_OPEN | FAN_EVENT_ON_CHILD,
-	    AT_FDCWD,
-	    argv[1]);
+	    AT_FDCWD, argv[1]);
 	if (m) {
 	    perror("error with 'fanotify_mark()'");
 	    return 2;
@@ -34,5 +26,4 @@ int main(int arg, char **argv) {
 	return 1;
     }
     return 0;
-
 }
